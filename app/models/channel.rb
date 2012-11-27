78d1f52873da2e6dd4edfbf4589e09b78e82ecf0
Eloy Espinaco <eloyesp@gmail.com>
2012-11-27 18:47:51 -0300

    Modularize fetching from channel.
    This way will be easier to test and to change SimpleRSS.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -7,33 +7,19 @@ class Channel < ActiveRecord::Base
 
   private
 
-    def parse_feed
-      if feed_url? and valid_feed_url?
-        @rss = SimpleRSS.parse(open(feed_url))
-        self.title =  @rss.title
-        self.url   =  @rss.link
-        self.description = @rss.description
-        @valid_feed = true
-      end
-    rescue SimpleRSSError
-      @valid_feed = false
+    def fetcher
+      @feed_fetcher ||= FeedFetcher.new feed_url
     end
 
-    def valid_feed_url?
-      self.feed_url = "http://" << feed_url unless feed_url =~ /^http/
-      if feed_url =~ URI::DEFAULT_PARSER.regexp[:ABS_URI]
-        true
-      else
-        @valid_feed = false unless feed_url.match URI::DEFAULT_PARSER.regexp[:ABS_URI]
-      end
+    def parse_feed
+      self.title       = fetcher.title
+      self.url         = fetcher.link
+      self.description = fetcher.description
     end
 
     def working_feed
-      errors.add(:feed_url, "The feed url is not working") unless valid_feed
+      errors.add(:feed_url, "is not working") unless fetcher.valid_feed?
     end
 
-    def valid_feed
-      @valid_feed ||= parse_feed
-    end
 end
 
---
+++ app/models/feed_fetcher.rb
@@ -0,0 +1,48 @@
+class FeedFetcher
+
+  delegate :title, :link, :description, to: :feed
+
+  def initialize feed_url
+    @feed_url = smart_add_url_protocol(feed_url)
+  end
+
+  def valid_feed?
+    return @valid_feed unless @valid_feed.nil?
+    feed # force fetching
+    @valid_feed
+  end
+
+  private
+
+    def feed
+      @feed ||= initialize_feed(@feed_url)
+    end
+
+    def initialize_feed url
+      if valid_url?
+        @feed = SimpleRSS.parse(open(url))
+        @valid_feed = true
+      end
+      @feed
+    rescue SimpleRSSError
+      @valid_feed = false
+      nil
+    end
+
+    def valid_url?
+      if @feed_url.match URI::DEFAULT_PARSER.regexp[:ABS_URI]
+        true
+      else
+        @valid_feed = false
+      end
+    end
+
+    def smart_add_url_protocol feed_url
+      unless feed_url[/^https?:\/\//]
+        feed_url = 'http://' + feed_url
+      end
+      feed_url
+    end
+
+end
+
